d50fa0ebab973026ba27ae9607b68b7bd996c741
Jacob Wesson <jacobwesson434@gmail.com>
2025-06-10 07:53:58 -0400

    update

--- lua/ethan/init.lua
+++ lua/ethan/init.lua
@@ -2,3 +2,4 @@ require("ethan.remap")
 require("ethan.set")
 require("ethan.lazy")
 require("ethan.gruvbox")
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,7 +19,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>t', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set('n', '<C-t>', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
